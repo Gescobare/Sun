700611f379c33e24921d91c55a3b0ee9107e6621
Gescobare <gustavoe9305@gmail.com>
2024-04-02 22:09:06 -0500

    El JSON con los municipios se ha cargado en una lista en la vista principal; se le agregaron estilos y capacidades responsivas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React, { Component, useEffect, useState } from "react"
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+
+
 
 function App() {
 
-    const [data, setData] = useState(null)
+  const [option, setOption] = useState([])
 
+  const url = "https://www.php.engenius.com.co/DatabaseIE.php"
+
+  const fetchData = () => {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
     var urlencoded = new URLSearchParams();
     urlencoded.append("User", "etraining");
-    urlencoded.append("Password", " ");
+    urlencoded.append("Password", "explorandoando2020%");
     urlencoded.append("option", "municipios");
 
     var requestOptions = {
@@ -18,21 +24,51 @@ function App() {
       body: urlencoded,
       redirect: 'follow'
     };
-    useEffect(() => {
-      fetch("https://www.php.engenius.com.co/DatabaseIE.php", requestOptions)
+
+    fetch(url, requestOptions)
       .then(response => response.json())
-      .then(res => console.log(res))
-      .then((data) => setData(data))
-    }, [])
-    
-  
-    return (
-      <div className="App">
-        
-        <div className="form-group">
-          <p>Hola</p>
+      .then(data => setOption(data.data))
+      .catch(error => console.error(error))
+  }
+
+  useEffect(() => {
+    fetchData(url)
+  }, [])
+
+  return (
+    <div className="content-wrapper">
+      <div className="page-header">
+        <h3 className="page-title"> Mi proyecto </h3>
+      </div>
+      <div className="row">
+        <div className="col-lg-12 grid-margin stretch-card">
+          <div className="card">
+            <div className="card-body">
+              <h4 className="card-title">Municipios</h4>
+              <div className="table-responsive">
+                <table class="table table-stripe">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Nombre del municipio</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {option.map((p) => <tr>
+                      {/*<td class="id-order"><Link to={`/show/${p.dane}`}>{p.dane}</Link></td>*/}
+                      <td class="id">{p.dane}</td>
+                      <td>{p.nombre}</td>
+                    </tr>
+                    )}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    )
+    </div>
+  );
 }
+
 export default App;
